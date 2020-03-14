c44ec059672bfa4ae5038797eaa3a30d5819f9b6
Randy Pantinople <randypantinople@yahoo.com>
2020-03-14 11:20:14 -0400

    Adding ThreadColor class

---
+++ ThreadColor.java
@@ -0,0 +1,12 @@
+package pantinople;
+
+public class ThreadColor {
+
+
+        public static final String ANSI_RED = "\u001B[31m";
+        public static final String ANSI_YELLOW = "\u001B[33m";
+        public static final String ANSI_BLUE = "\u001B[34m";
+
+
+    }
+
